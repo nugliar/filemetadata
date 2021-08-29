4926e8b04cb93e818b15e4ec3d4c8f701f074d9e
nugliar <hashan.hall@gmail.com>
2021-08-29 10:36:28 -0500

    add post routes

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multiparty = require('connect-multiparty');
 require('dotenv').config()
 
-var app = express();
+const app = express();
+const multipartyMiddleware = multiparty();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +13,30 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', multipartyMiddleware, function(req, res) {
+  console.log(req.files);
+  if (req.files) {
+    const file = req.files.upfile
 
+    if (!file.originalFilename) {
+      res.json({error: 'No file uploaded'});
+    }
+    else {
+      res.json({
+        name: file.originalFilename,
+        type: file.type,
+        size: file.size
+      });
+    }
+  }
+  else {
+    res.json({error: 'No file uploaded'})
+  }
+})
 
+app.use(function(req, res) {
+  res.send('Page not found')
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
